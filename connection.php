50146333228a7627bd69454146a4038f19ebaedb
buzbunny <109228039+buzbunny@users.noreply.github.com>
2024-04-11 16:33:59 +0100

    Update connection

--- connection.php
+++ connection.php
@@ -1,4 +1,4 @@
 <?php
-$con=mysqli_connect("localhost","root","","store") or die(mysqli_error($con));
+$con=mysqli_connect("localhost","root","Vcnm1H&w","store") or die(mysqli_error($con));
 //$con=mysqli_connect("localhost","root","","store") or die(mysqli_error($con));
 ?>
